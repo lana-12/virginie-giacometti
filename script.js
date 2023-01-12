a1a2b72193f2aedbc59c38370b0891b98b503586
Virginie Giacometti <lana.12@wanadoo.fr>
2023-01-12 13:33:08 +0100

    modal +cv

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("Site en cours de Construction !! Merci de votre compréhension ;)");
+// START JQuery
+$(document).ready(()=>{
+    console.log('Jquery bien installé au cas ou !!');
+    
+    
+
+}); // end Jquery
+// alert(" Site en cours de Construction !! Merci de votre compréhension ;)");
+
+// première méthode avec <body class="..." onload="myModal()" >
+// function myModal(){
+//     var myModal = new bootstrap.Modal(document.getElementById("myModal"));
+//     myModal.show(myModal);
+// }
+
+window.onload = function () {
+    console.log("function called...");
+    var myModal = new bootstrap.Modal(document.getElementById("myModal"));
+    myModal.show();
+};
