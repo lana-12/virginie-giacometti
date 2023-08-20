ecb98e076c3cafe3537424c28ad34341ff23d7ec
Virginie Giacometti <lana.12@wanadoo.fr>
2023-08-20 10:58:07 +0200

    add modal

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+let maZone = document.getElementById("stage");
+
+
+maZone.addEventListener("click", function() {
+    console.log('hello')
+    $('#myModal').modal('show');
+});
+// maZone.addEventListener("click", function() {
+//     console.log('hello')
+//     let monImage = document.createElement("img");
+//     maZone.appendChild(monImage);
+//     monImage.src = "../img/diplome.jpg";
+//     monImage.style.display ='block';
+//     // $('#myModal').modal('show');
+// });
